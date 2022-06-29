84f69fc0ad9e5c5d358d3a980f638f1fecfcc490
esmanda3w <esmanda3w@gmail.com>
2022-06-29 09:44:12 +0800

    debug clearml

--- mount/st_covost2/st1/run.py
+++ mount/st_covost2/st1/run.py
@@ -14,8 +14,8 @@ task.set_base_docker('dleongsh/espnet:202205-torch1.10-cu113-runtime')
 task.execute_remotely(queue_name='compute', clone=False, exit_process=True)
 
 # download dataset
-clearml_dataset = Dataset.get(dataset_id='9904e5f0fe2e4ddcb56defe61d3c1b0a')
-dataset_path = clearml_dataset.get_local_copy()
+# clearml_dataset = Dataset.get(dataset_id='9904e5f0fe2e4ddcb56defe61d3c1b0a')
+# dataset_path = clearml_dataset.get_local_copy()
 # "ds.clearml9927dhdwu278e8/espnet/"
 
 # # download model
@@ -24,30 +24,33 @@ dataset_path = clearml_dataset.get_local_copy()
 
 data_tag = "test_clearml"
 
-shutil.copytree('../../st_covost2', '/workspace/espnet/egs2/st_covost2')
+print(os.listdir("../../st_covost2"))
+print(os.listdir("/workspace/espnet/egs2/"))
 
-subprocess.run([
-    './run.sh',
-    '--data_folder', dataset_path,
-    '--data_tag', data_tag,
-    # '--pretrained_model', model_path,
-    ])
+# shutil.copytree('../../st_covost2', '/workspace/espnet/egs2/st_covost2')
 
-output_dirs = {
-    'lm_exp': "./exp/st_covost2/test_clearml/lm_exp",
-    'lm_stats_dir': "./exp/st_covost2/test_clearml/lm_stats",
-    'st_exp': "./exp/st_covost2/test_clearml/st_exp",
-    'st_stats_dir': "./exp/st_covost2/test_clearml/st_stats",
-}
+# subprocess.run([
+#     './run.sh',
+#     '--data_folder', dataset_path,
+#     '--data_tag', data_tag,
+#     # '--pretrained_model', model_path,
+#     ])
 
-for key in output_dirs.keys():
-    dirx = output_dirs[key]
-    for root, _, filenames in os.walk(dirx):
-        for filename in filenames:
+# output_dirs = {
+#     'lm_exp': "./exp/st_covost2/test_clearml/lm_exp",
+#     'lm_stats_dir': "./exp/st_covost2/test_clearml/lm_stats",
+#     'st_exp': "./exp/st_covost2/test_clearml/st_exp",
+#     'st_stats_dir': "./exp/st_covost2/test_clearml/st_stats",
+# }
 
-            # to avoid uploading all the epoch weights
-            if filename.endswith('epoch.pth'):
-                continue
+# for key in output_dirs.keys():
+#     dirx = output_dirs[key]
+#     for root, _, filenames in os.walk(dirx):
+#         for filename in filenames:
 
-            filepath = os.path.join(root, filename)
-            task.upload_artifact(f'{key}_{filename}', artifact_object=filepath)
+#             # to avoid uploading all the epoch weights
+#             if filename.endswith('epoch.pth'):
+#                 continue
+
+#             filepath = os.path.join(root, filename)
+#             task.upload_artifact(f'{key}_{filename}', artifact_object=filepath)
