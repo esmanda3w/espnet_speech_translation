027f695ce1fa2a07b7a5af0459d74acbfe7b9d69
esmanda3w <esmanda3w@gmail.com>
2022-07-28 17:19:37 +0800

    update java pipeline

--- mount/java/asr1/run.sh
+++ mount/java/asr1/run.sh
@@ -10,15 +10,13 @@ log() {
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
 
-cwd=$PWD
-
 cd /workspace/espnet/egs2/java/asr1
+
+##### PRETRAINED MODEL SETUP #####
 
 pretrain_model=jv_openslr35
 bpe_model=bpe_unigram1000
 inference_asr_model=valid.acc.best.pth
-
-##### SETTING UP ASR #####
 
 # Creating model's token list
 pyscripts/utils/make_token_list_from_config.py /models/${pretrain_model}/config.yaml
@@ -28,53 +26,50 @@ cp /models/${pretrain_model}/tokens.txt data/token_list/${bpe_model}/
 # Copying the bpe model
 cp /models/${pretrain_model}/${bpe_model}/bpe.model data/token_list/${bpe_model}/
 
-##########################
+##################################
 
+##### GENERAL CONFIG #####
+
+# Language related
 src_lang=id
 tgt_lang=en
 
-is_low_resource=true
-
-if [ ${is_low_resource} = true ]; then
-    speed_perturb_factors="0.8 0.9 1.0 1.1 1.2"
-else
-    speed_perturb_factors="0.9 1.0 1.1"
-fi
-
+# Experiment folder related
 main_folder="java/id"
-sub_folder="lowresspeedperturb_finetune_2gb_clean"
-train_data_folder=/datasets/id_data_2gb_cleaned
-test_data_folder=/datasets/test_id_data_1gb_cleaned
-data_tag=2gb_clean
+sub_folder="lm_finetune_6gb_clean"
+
+# Data related
+train_data_folder=/datasets/id_data_6gb_cleaned
+test_data_folder="/datasets/test_id_data_1gb_cleaned"
+data_tag=6gb_cleaned
 
 train_set=train_${data_tag}
 train_dev=valid_${data_tag}
 test_sets="test_${data_tag}"
 
+# Train and decode config related
 asr_config=conf/train_asr.yaml
 inference_config=conf/decode_asr.yaml
 
-package_model=false
-package_model_name=
+# LM related
+use_lm=true
 
-local_run=false
+# Package model related
+package_model=true
+package_model_name=lm_finetune_6gb_clean_asr
 
 . utils/parse_options.sh || exit 1;
 
-if "${local_run}"; then
-    cwd=/mount
-fi
+##########################
 
-# Run stage 1-4, 9-13
-for stage in 1 2 3 4 9 10 11 12 13; do
-# for stage in 2 3 4 9 10 11 12 13; do
+# Run stage 1-4, 6-8 (LM), 9-13
+for stage in 1 2 3 4 6 7 8 9 10 11 12 13; do
     ./asr.sh \
         --stage ${stage} \
         --stop_stage ${stage} \
         --local_data_opts "--src_lang ${src_lang} --tgt_lang ${tgt_lang} --train_data_folder ${train_data_folder} --test_data_folder ${test_data_folder} --data_tag ${data_tag}" \
         --ngpu 1 \
-        --speed_perturb_factors "${speed_perturb_factors}" \
-        --use_lm false \
+        --use_lm ${use_lm} \
         --token_type bpe \
         --nbpe 1000 \
         --feats_type raw \
@@ -84,14 +79,16 @@ for stage in 1 2 3 4 9 10 11 12 13; do
         --valid_set ${train_dev} \
         --test_sets ${test_sets} \
         --lm_train_text "data/${train_set}/text" \
-        --lm_exp "${cwd}/exp/${main_folder}/${sub_folder}/lm_exp" \
-        --lm_stats_dir "${cwd}/exp/${main_folder}/${sub_folder}/lm_stats" \
-        --asr_exp "${cwd}/exp/${main_folder}/${sub_folder}/asr_exp" \
-        --asr_stats_dir "${cwd}/exp/${main_folder}/${sub_folder}/asr_stats" \
-        --pretrained_model "/models/${pretrain_model}/${inference_asr_model}" \
+        --lm_exp "/mount/exp/${main_folder}/${sub_folder}/lm_exp" \
+        --lm_stats_dir "/mount/exp/${main_folder}/${sub_folder}/lm_stats" \
+        --asr_exp "/mount/exp/${main_folder}/${sub_folder}/asr_exp" \
+        --asr_stats_dir "/mount/exp/${main_folder}/${sub_folder}/asr_stats" \
         --inference_asr_model "${inference_asr_model}" \
+        --pretrained_model "/models/${pretrain_model}/${inference_asr_model}" \
         --ignore_init_mismatch "true"
 done
+
+##### PACKAGE MODEL #####
 
 if "${package_model}"; then
 
@@ -113,4 +110,13 @@ if "${package_model}"; then
     # Copy model's bpe.model
     cp data/token_list/${bpe_model}/bpe.model ${model_dir}/${bpe_model}
 
+    if [ "${use_lm}" ]; then
+        lm_model=valid.loss.ave.pth
+        mkdir -p ${model_dir}/lm
+        cp /mount/exp/${main_folder}/${sub_folder}/lm_exp/config.yaml ${model_dir}/lm
+        cp /mount/exp/${main_folder}/${sub_folder}/lm_exp/${lm_model} ${model_dir}/lm
+    fi
+
 fi
+
+#########################
